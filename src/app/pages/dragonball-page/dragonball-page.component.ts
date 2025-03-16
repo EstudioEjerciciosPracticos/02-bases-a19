2a0306d2832d8c03c3eeb616958add5eef835253
Felipe Naranjo <wfnaranjo17@gmail.com>
2025-03-16 17:40:19 -0500

    Seccion 5 iteraciones con for

---
+++ src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
+@Component({
+  selector: 'app-dragonball-page',
+  imports: [CommonModule],
+  templateUrl: './dragonball-page.component.html',
+  styleUrl: './dragonball-page.component.css'
+})
+export class DragonballPageComponent {
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 3, name: 'Piccolo', power: 3000 }
+  ]);
+
+}
